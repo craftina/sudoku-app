fdb93de5ba5f8492ce8fe1a24e4087c1f71c4a45
craftina <hristina.rum.hristova@gmail.com>
2025-06-19 16:36:38 +0300

    adding solve button, post request in the api for solving , encoding utility function

--- src/app/services/sudoku-api.ts
+++ src/app/services/sudoku-api.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BoardModel } from '../models/board.model';
 import { Difficulty } from '../models/difficulty.model';
+import { encodeBoard } from '../utils/encode-board';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,12 @@ export class SudokuApi {
 
   getBoard(difficulty: Difficulty): Observable<BoardModel> {
     return this.http.get<BoardModel>(this.baseUrl + "board?difficulty=" + difficulty)
+  }
+
+  solveBoard(board: BoardModel['board']): Observable<any> {
+    const body = encodeBoard(board);
+    return this.http.post(`${this.baseUrl}solve`, body, {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    });
   }
 }
